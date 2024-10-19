208f49e0589039e1f3f4eb6fdd0e69691e69776a
emmamayne23 <emmamayne23@gmail.com>
2024-10-19 19:38:39 -0400

    Making the first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+
+    extend: {
+      colors: {
+        'ash': '#8d99af'
+      },
+    },
+  },
+  plugins: [],
+}
+
